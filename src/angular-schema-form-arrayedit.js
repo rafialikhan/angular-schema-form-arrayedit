40ba53a46420418c5a689bfda6cd64811026a51c
Rafi Ali Khan <rafialikhan@gmail.com>
2016-12-30 04:45:48 +0530

    overwriting the default array schema-form

--- src/angular-schema-form-arrayedit.js
+++ src/angular-schema-form-arrayedit.js
@@ -1,27 +1,12 @@
 angular.module('schemaForm').config(
-    ['schemaFormProvider', 'schemaFormDecoratorsProvider', 'sfPathProvider',
-        function(schemaFormProvider,  schemaFormDecoratorsProvider, sfPathProvider) {
-
-          var arrayedit = function(name, schema, options) {
-            if (schema.type === 'array' && schema.format === 'arrayedit') {
-              var f = schemaFormProvider.stdFormObj(name, schema, options);
-              f.key  = options.path;
-              f.type = 'arrayedit';
-              options.lookup[sfPathProvider.stringify(options.path)] = f;
-              return f;
-            }
-          };
-
-          schemaFormProvider.defaults.string.unshift(arrayedit);
-          //Add to the bootstrap directive
+    ['schemaFormProvider', 'schemaFormDecoratorsProvider', 'sfPathProvider', 'sfBuilderProvider',
+        function(schemaFormProvider, schemaFormDecoratorsProvider,
+         sfPathProvider, sfBuilderProvider) {
+          //Add Mapping
           schemaFormDecoratorsProvider.addMapping(
-              'bootstrapDecorator',
-              'arrayedit',
-              'directives/decorators/bootstrap/arrayedit/angular-schema-form-arrayedit.html'
-          );
-          schemaFormDecoratorsProvider.createDirective(
-              'arrayedit',
-              'directives/decorators/bootstrap/arrayedit/angular-schema-form-arrayedit.html'
+            'bootstrapDecorator',
+            'array',
+            'directives/decorators/bootstrap/arrayedit/angular-schema-form-arrayedit.html'
           );
         }
-    ])
+    ]);
